554328ba9e716401ddac0292fc49f6459c2ae7d7
Meagan Chase <meaganchase814@yahoo.com>
2018-11-29 11:55:55 -0500

    made git repository

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -4,8 +4,15 @@ class DocsController < ApplicationController
     def show
     end
     def new
+        @doc = Doc.new
     end
     def create
+        @doc = Doc.new(doc_params)
+        if @doc.save
+            redirect_to @doc
+        else
+            render 'new'
+        end
     end
     def edit
     end
@@ -18,6 +25,7 @@ class DocsController < ApplicationController
         def find_doc
         end
         def doc_params
+            params.require(:doc).permit(:title, :content)
         end
-    end
+    
 end
